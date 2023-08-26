927dc3b8542743ab6e9229f749e8dd6eb9905532
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-08-26 15:16:42 -0300

    App (style): run code formatter and linter scripts

--- src/types/storybook.ts
+++ src/types/storybook.ts
@@ -9,15 +9,19 @@ export type PlayFunctionParameter<Component extends SvelteComponent> =
   Parameters<Extract<StoryObj<Component>["play"], Function>>[0] & {
     args: {
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      [P in keyof ComponentEvents<Component> as `event_${Extract<P, string>}`]: jest.Mock<
+      [P in keyof ComponentEvents<Component> as `event_${Extract<
+        P,
+        string
+      >}`]: jest.Mock<
         (event: ComponentEvents<Component>[P]) => any | Promise<any>
       >;
     };
   };
 
-export type TemplateObj<TMeta extends Meta<any>, RenderComponent extends SvelteComponent> = Partial<
-  Omit<StoryObj<RenderComponent>, "args">
-> & {
+export type TemplateObj<
+  TMeta extends Meta<any>,
+  RenderComponent extends SvelteComponent
+> = Partial<Omit<StoryObj<RenderComponent>, "args">> & {
   args?: StoryObj<RenderComponent>["args"];
 } & {
   // Save the TMeta and RenderComponent type to be used at the StoryTemplate
