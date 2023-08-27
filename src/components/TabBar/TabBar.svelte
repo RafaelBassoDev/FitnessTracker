4e6a80f32cc33565eeb9850f094b662fbf22303f
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-08-27 15:58:48 -0300

    TabBar (feat): add style to tab bar

--- src/components/TabBar/TabBar.svelte
+++ src/components/TabBar/TabBar.svelte
@@ -4,3 +4,18 @@
   <button class="tab-button">Settings</button>
 </div>
 
+<style>
+  .tab-bar {
+    display: flex;
+    justify-content: center;
+    background-color: #f0f0f0;
+    margin: 0;
+    position: fixed;
+    padding-bottom: 1em;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    height: 60px;
+    box-shadow: 0px -2px 5px rgba(0, 0, 0, 0.1);
+  }
+</style>
