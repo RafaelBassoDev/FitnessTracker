23537c617bcbb3c2e96ff29e46adec96482f708c
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-08-29 11:55:17 -0300

    App (refactor): remove header component

--- src/App.svelte
+++ src/App.svelte
@@ -1,35 +1,9 @@
 <script lang="ts">
-  import Router, { push } from "svelte-spa-router";
   import { conditionsFailHandler, routeLoadingHandler, routes } from "./routes";
-  import LoggedInUser from "$stores/LoggedInUser";
-  import lunaImg from "$assets/Luna.jpg";
-  import Header from "$components/Header/Header.svelte";
-  import { tick } from "svelte";
+  import Router from "svelte-spa-router";
   import TabBar from "$components/TabBar/TabBar.svelte";
-
-  const userMock = {
-    name: "João Silva",
-    image: lunaImg,
-  };
 </script>
 
-<header>
-  <Header
-    user={$LoggedInUser}
-    on:login={() => ($LoggedInUser = userMock)}
-    on:logout={() => {
-      push("/")
-        .then(async () => {
-          await tick();
-          $LoggedInUser = null;
-        })
-        .catch((e) => {
-          throw e;
-        });
-    }}
-    on:createAccount={() => ($LoggedInUser = userMock)}
-  />
-</header>
 <main>
   <Router
     {routes}
