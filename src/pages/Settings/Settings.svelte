ef5ba3f2c2558fadf67346a1dc39d6e596cbcdbf
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-08-29 14:35:25 -0300

    Settings (feat): add stylized header

---
+++ src/pages/Settings/Settings.svelte
@@ -0,0 +1,24 @@
+<div class="header">
+  <h1>Ajustes</h1>
+</div>
+
+<style lang="scss">
+  .header {
+    display: flex;
+    justify-content: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    padding-top: 3em;
+    width: 100%;
+    background-color: #007aff;
+
+    h1 {
+      color: white;
+      font-family: sans-serif;
+      font-size: 22px;
+      font-weight: bold;
+      margin: 8;
+    }
+  }
+</style>
