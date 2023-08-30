d6b352365142d3f39df78637369876f6344de530
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-08-30 18:51:49 -0300

    Settings (refactor): add SettingGroups and SettingGroupRows to body

--- src/pages/Settings/Settings.svelte
+++ src/pages/Settings/Settings.svelte
@@ -1,24 +1,31 @@
-<div class="header">
-  <h1>Ajustes</h1>
+<script lang="ts">
+  import SettingsGroup from "./SettingsGroup.svelte";
+  import SettingsGroupRow from "./SettingsGroupRow.svelte";
+</script>
+
+<div class="container">
+  <SettingsGroup title="Horários">
+    <SettingsGroupRow label="Acordar" />
+    <SettingsGroupRow label="Dormir" />
+    <SettingsGroupRow label="Ativar Notificações" />
+  </SettingsGroup>
+
+  <SettingsGroup title="Geral">
+    <SettingsGroupRow label="Ingestão Diária" />
+    <SettingsGroupRow label="Utilizar Volume Sugerido por Lembrete" />
+    <SettingsGroupRow label="Volume por Lembrete" />
+  </SettingsGroup>
 </div>
 
 <style lang="scss">
-  .header {
-    display: flex;
-    justify-content: center;
-    position: fixed;
+  .container {
+    position: absolute;
     top: 0;
+    right: 0;
+    bottom: 0;
     left: 0;
-    padding-top: 3em;
-    width: 100%;
-    background-color: #007aff;
-
-    h1 {
-      color: white;
-      font-family: sans-serif;
-      font-size: 22px;
-      font-weight: bold;
-      margin: 8;
-    }
+    padding: 2.5em 0 0 0;
+    font-family: Verdana;
+    background-color: #f2f1f6;
   }
 </style>
