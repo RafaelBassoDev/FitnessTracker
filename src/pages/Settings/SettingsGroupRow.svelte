e11e6ff10368a007b93ca49f6d5c628e1677dca0
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-08-30 18:23:40 -0300

    SettingsGroupRow (feat): add style to component

--- src/pages/Settings/SettingsGroupRow.svelte
+++ src/pages/Settings/SettingsGroupRow.svelte
@@ -9,3 +9,17 @@
   <span class="row-label">{label}</span>
   <slot />
 </div>
+
+<style lang="scss">
+  .settings-group-row {
+    display: flex;
+    align-items: center;
+    padding: 0.3em 0;
+    margin: 8px 0;
+  }
+
+  .row-label {
+    font-size: 16px;
+    flex: 1;
+  }
+</style>
