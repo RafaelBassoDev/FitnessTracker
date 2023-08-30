ed08536c186ff9a157be0d395c21af5fce07d930
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-08-30 18:12:54 -0300

    SettingsGroup (feat): add 'title' attribute to component

---
+++ src/pages/Settings/SettingsGroup.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  /**
+   * Defines the group title.
+   *
+   * If no value is passed, the group will work nonetheless but without a title.
+   */
+  export let title: string = undefined;
+</script>
