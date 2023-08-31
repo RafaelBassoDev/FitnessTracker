0e0569766c16f1317bb111387414e4b67eb1d97f
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-08-31 17:30:04 -0300

    UserSettings (feat): add constructor with 'Writable' parameters

--- src/stores/UserSettings.ts
+++ src/stores/UserSettings.ts
@@ -1,3 +1,16 @@
-class UserSettings {}
+import { writable, type Writable } from "svelte/store";
+
+class UserSettings {
+  constructor(
+    public startHour: Writable<string> = writable("08:00"),
+    public endHour: Writable<string> = writable("23:00"),
+    public enableNotifications: Writable<boolean> = writable(true),
+    public dailyVolume: Writable<number> = writable(2900),
+    public minDailyVolume: Writable<number> = writable(100),
+    public maxDailyVolume: Writable<number> = writable(6000),
+    public isAutomaticVolumeEnabled: Writable<boolean> = writable(true),
+    public selectedVolume: Writable<number | null> = writable(0)
+  ) {}
+}
 
 export const userSettings = new UserSettings();
