4342bbb95f95a4d41fb5e947820534771fb811f6
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-09-01 19:04:39 -0300

    UserSettings (reafactor): add 'Time' parameters to constructor

--- src/stores/UserSettings.ts
+++ src/stores/UserSettings.ts
@@ -1,10 +1,11 @@
 import { get, writable, type Writable } from "svelte/store";
 import { createVolume } from "$helpers/Volume";
+import Time from "$helpers/Time";
 
 class UserSettings {
   constructor(
-    public startHour: Writable<string> = writable("08:00"),
-    public endHour: Writable<string> = writable("23:00"),
+    private wakeUpTime: Time = new Time(8, 0),
+    private sleepTime: Time = new Time(22, 0),
     public enableNotifications: Writable<boolean> = writable(true),
     public dailyVolume: Writable<number> = writable(2900),
     public minDailyVolume: Writable<number> = writable(100),
