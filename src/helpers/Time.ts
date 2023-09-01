2607a56046aee43beac355a2fd859c86a2f9e008
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-09-01 18:55:41 -0300

    Time (feat): add methods to set and retrieve formatted time

--- src/helpers/Time.ts
+++ src/helpers/Time.ts
@@ -7,6 +7,18 @@ export default class Time {
     this.minutes = minutes;
   }
 
+  public setTime(unformattedTime: string) {
+    const formattedTime = unformattedTime.split(":");
+    this.hour = Number(formattedTime[0]);
+    this.minutes = Number(formattedTime[1]);
+  }
+
+  public getFomattedTime(): string {
+    const formattedHour = this.hour.toString().padStart(2, "0");
+    const formattedMinutes = this.minutes.toString().padStart(2, "0");
+    return `${formattedHour}:${formattedMinutes}`;
+  }
+
   public getTotalMinutes(): number {
     return this.hour * 60 + this.minutes;
   }
