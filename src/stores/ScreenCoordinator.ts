ecc06ddd29bdb599bf9fc58d198a3e1b027a6108
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-09-02 18:30:38 -0300

    ScreenCoordinator (feat): add constructor and get/set methods

---
+++ src/stores/ScreenCoordinator.ts
@@ -0,0 +1,16 @@
+import { Screen } from "$helpers/Screen";
+
+export class ScreenCoordinator {
+  private _currentScreen: Screen;
+
+  constructor(initialScreen: Screen) {
+    this._currentScreen = initialScreen;
+  }
+  get currentScreen(): Screen {
+    return this._currentScreen;
+  }
+
+  set currentScreen(newValue: Screen) {
+    this._currentScreen = newValue;
+  }
+}
