e3f13e0ca77972b2558f35fea7b074ee32426ec2
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-09-02 15:16:34 -0300

    TabBarButton (feat): add content and style

---
+++ src/components/TabBar/TabBarButton.svelte
@@ -0,0 +1,13 @@
+<button class="tab-button">
+  <slot />
+</button>
+
+<style lang="scss">
+  .tab-button {
+    border: none;
+    background-color: transparent;
+    font-size: 16px;
+    padding: 10px;
+    margin: 0 5px;
+  }
+</style>
