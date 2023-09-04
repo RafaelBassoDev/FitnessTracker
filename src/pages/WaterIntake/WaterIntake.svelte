a5f12cfdec88a448e58c639e6ec03b4fc7481707
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-09-04 11:31:03 -0300

    WaterIntake (feat): add water consumption label and button

--- src/pages/WaterIntake/WaterIntake.svelte
+++ src/pages/WaterIntake/WaterIntake.svelte
@@ -1,1 +1,15 @@
-<h1>Water Intake</h1>
+<div class="container">
+  <div class="info-display">
+    <div class="primary">1200</div>
+    <div class="secondary">DE</div>
+    <div class="primary">2900<span class="detail">ml</span></div>
+  </div>
+  <div class="input-container">
+    <button
+      class="add-button"
+      on:click={() => {
+        // somar agua
+      }}>Beber Água</button
+    >
+  </div>
+</div>
