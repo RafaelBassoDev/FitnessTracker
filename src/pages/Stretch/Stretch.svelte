2a0b345d322e44817b9def53465a10dfb534dbbd
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-09-04 17:06:55 -0300

    Stretch (feat): add page body

--- src/pages/Stretch/Stretch.svelte
+++ src/pages/Stretch/Stretch.svelte
@@ -1,1 +1,20 @@
-<h1>Stretch</h1>
+<script lang="ts">
+  let count = 0;
+</script>
+
+<div class="container">
+  <div class="info-display">
+    <div class="primary">PAUSAS</div>
+    <div class="secondary">{count}</div>
+  </div>
+  <div class="input-container">
+    <button
+      class="add-button"
+      on:click={() => {
+        count++;
+      }}
+    >
+      +
+    </button>
+  </div>
+</div>
