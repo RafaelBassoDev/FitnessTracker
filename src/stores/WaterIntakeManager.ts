424c779c471f80d22533371127eb3d9cb369b0d7
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-09-04 11:59:19 -0300

    WaterIntakeManager (feat): add getter and setter for attribute

--- src/stores/WaterIntakeManager.ts
+++ src/stores/WaterIntakeManager.ts
@@ -1,3 +1,17 @@
-class WaterIntakeManager {}
+class WaterIntakeManager {
+  private _totalConsumedWaterVolume: number;
+
+  public get currentIntakeVolume(): number {
+    return this._totalConsumedWaterVolume;
+  }
+
+  public set currentIntakeVolume(volume: number) {
+    if (isNaN(volume)) {
+      return;
+    }
+
+    this._totalConsumedWaterVolume = volume;
+  }
+}
 
 export const waterIntakeManager = new WaterIntakeManager();
