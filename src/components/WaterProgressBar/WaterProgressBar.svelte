83bb66b9d82f78cb46df283c0c3d2f31dcfa51a4
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-09-04 11:25:26 -0300

    WaterProgressBar (feat): add component props

---
+++ src/components/WaterProgressBar/WaterProgressBar.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { clamp } from "$helpers/MathUtils";
+  // Water wave effect
+  // https://codepen.io/muhammad_mabrouk/pen/gOPNLyo
+
+  /**
+   * What is the current progress of the bar?
+   *
+   * This value is limited to numbers between 0 and 100.
+   */
+  export let progress = 0;
+
+  $: height = clamp(Math.round(progress), 0, 100);
+</script>
