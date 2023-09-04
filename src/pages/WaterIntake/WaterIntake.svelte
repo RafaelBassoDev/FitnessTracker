ea6e3f90b1f88a63d537ef1fc771d55968f3faba
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-09-04 11:31:51 -0300

    WaterIntake (feat): add style to page body

--- src/pages/WaterIntake/WaterIntake.svelte
+++ src/pages/WaterIntake/WaterIntake.svelte
@@ -13,3 +13,53 @@
     >
   </div>
 </div>
+
+<style lang="scss">
+  .container {
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-family: Verdana;
+    gap: 15em;
+    height: 100vh;
+    background-color: transparent;
+    overflow: hidden;
+  }
+
+  .info-display {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-weight: bolder;
+
+    .primary {
+      font-size: 36px;
+    }
+
+    .secondary {
+      font-size: 20px;
+      margin: 0 10px;
+    }
+
+    .detail {
+      font-size: 28px;
+    }
+  }
+
+  .add-button {
+    background-color: #007aff;
+    color: white;
+    border: none;
+    padding: 10px 20px;
+    font-size: 18px;
+    border-radius: 12px;
+    padding: 1em;
+    font-weight: bold;
+  }
+</style>
